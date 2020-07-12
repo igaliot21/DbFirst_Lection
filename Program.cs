7cf3b8283d912cb5fc34e6f38ee65a3f8f856c78
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-07-12 19:14:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbFirst
+{
+    public enum Level : byte { 
+        Beginner = 1,
+        Intermidiate = 2,
+        Adavanced = 3
+    }
+    class Program
+    {
+        static  void Main(string[] args)
+        {
+            var course = new Course();
+            // course.Level = CourseLevel.Beginner;
+            course.Level = Level.Beginner;
+            //foreach (var c in new PlutoDbContext().GetCourses()) Console.WriteLine(c.Title);
+        }
+    }
+}
